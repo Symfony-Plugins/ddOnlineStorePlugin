a766f65cf8b76c9955f47894f637871f60d4ea32
songecko <songecko@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-10-22 16:52:29 -0500

    git-svn-id: http://svn.symfony-project.com/plugins/ddOnlineStorePlugin/trunk@31228 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/ddOnlineStoreProduct/templates/_list_pager_extra_content.php
+++ modules/ddOnlineStoreProduct/templates/_list_pager_extra_content.php
@@ -3,15 +3,25 @@
 <?php endif; ?>
 
 <?php if(isset($attributes['includeSort']) && $attributes['includeSort'] === true): ?>
-<form name="pager_sort_by" id="pager_sort_by" action="#" method="get">
+<form name="pager_sort_by" id="pager_sort_by" action="<?php echo url_for('localized_homepage') ?>" method="get">
 	<label for="sort_by">Sort By:</label>
 	<select name="sort" id="sort_by">
-    	<option value="is_featured|desc">Featured first</option>
-        <option value="category|asc">Category</option>
-        <option value="price|asc">Price (min to max)</option>
-        <option value="price|desc">Price (max to min)</option>
-        <option value="capacity|asc">Storage capaticty (min to max)</option>
-        <option value="capacity|desc">Storage capaticty (max to min)</option>
+    	<option value="is_featured|desc"<?php echo (implode('|', $attributes['sort']->getRawValue()) == 'is_featured|desc')?' selected="selected"':'' ?>>Featured first</option>
+        <option value="category_id|asc"<?php echo (implode('|', $attributes['sort']->getRawValue()) == 'category_id|asc')?' selected="selected"':'' ?>>Category</option>
+        <option value="price|asc"<?php echo (implode('|', $attributes['sort']->getRawValue()) == 'price|asc')?' selected="selected"':'' ?>>Price (min to max)</option>
+        <option value="price|desc"<?php echo (implode('|', $attributes['sort']->getRawValue()) == 'price|desc')?' selected="selected"':'' ?>>Price (max to min)</option>
+        <option value="capacity|asc"<?php echo (implode('|', $attributes['sort']->getRawValue()) == 'capacity|asc')?' selected="selected"':'' ?>>Storage capaticty (min to max)</option>
+        <option value="capacity|desc"<?php echo (implode('|', $attributes['sort']->getRawValue()) == 'capacity|desc')?' selected="selected"':'' ?>>Storage capaticty (max to min)</option>
 	</select>
 </form>
+
+<script type="text/javascript">
+<!--
+$(document).ready(function(){
+	$('#sort_by').change(function(){
+		$('#pager_sort_by').submit();
+	});
+});
+//-->
+</script>
 <?php endif; ?>
