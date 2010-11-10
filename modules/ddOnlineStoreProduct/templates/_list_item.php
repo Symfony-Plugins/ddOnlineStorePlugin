250961132e0abbddd7ae7ec4495e4443d65d6aec
songecko <songecko@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-11-10 09:23:59 -0600

    - now, the product details shows in a popup for jquery fancybox
    git-svn-id: http://svn.symfony-project.com/plugins/ddOnlineStorePlugin/trunk@31348 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/ddOnlineStoreProduct/templates/_list_item.php
+++ modules/ddOnlineStoreProduct/templates/_list_item.php
@@ -1,5 +1,5 @@
 <li<?php echo ($i%4 == 0)?' class="last"':''?>>
-	<a href="javascript:void(0);" <?php /*onclick="fadeIn('product_detail_<?php echo $product->getId() ?>');"*/?><?php echo ($product->getIsFeatured())?' class="special"':'' ?>>
+	<a href="#product_detail_<?php echo $product->getId() ?>" class="product_item<?php echo ($product->getIsFeatured())?' special':'' ?>" id="product_<?php echo $product->getId() ?>">
 		<?php if($mainImage = $product->getMainImage()): ?> 
 		<img src="<?php echo $mainImage->getImageSrc('image_name', 'thumb') ?>" alt="<?php echo $product->getName() ?>" />
 		<?php endif; ?> 
@@ -9,4 +9,26 @@
             <span class="product_price">$ <?php echo $product->getPrice() ?></span> 
 		</span> 
 	</a>
+	
+	<div style="display:none;">
+	    <div class="popup_content" id="product_detail_<?php echo $product->getId() ?>">
+	        <div class="popup_content_wrapper">
+	            <a href="javascript:void(0);" onclick="$.fancybox.close()" class="popup_close">X</a>
+	            <div class="popup_left_column">
+	                <img src="<?php echo $mainImage->getImageSrc('image_name', 'detail') ?>" alt="" />
+	            </div>
+	            <div class="popup_right_column">
+	                <p><b><?php echo $product->getName() ?></b></p>
+	                <p><span class="popup_price">$ <?php echo $product->getPrice() ?></span></p>
+	                <p><?php echo $product->getDescription() ?></p>
+	                <?php if($product->user->VendorProfile): ?>
+	                <p>
+	                	<b>Sold by:</b> 
+	                	<a href="<?php echo url_for('vendor_show', $product->user->VendorProfile) ?>" class="brand"><?php echo $product->user->VendorProfile->getBusinessName() ?></a>
+	                </p>
+	                <?php endif; ?>
+	            </div>
+	        </div>
+	    </div>
+	</div>
 </li>
