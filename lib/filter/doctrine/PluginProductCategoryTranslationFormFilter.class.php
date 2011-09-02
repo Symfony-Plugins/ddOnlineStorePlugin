8719c3edad37be76c33bf21ef11a7f5f6cb65a9f
songecko <songecko@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2011-09-02 16:34:52 -0500

    git-svn-id: http://svn.symfony-project.com/plugins/ddOnlineStorePlugin/trunk@32985 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/filter/doctrine/PluginProductCategoryTranslationFormFilter.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * PluginProductCategoryTranslation form.
+ *
+ * @package    ##PROJECT_NAME##
+ * @subpackage filter
+ * @author     ##AUTHOR_NAME##
+ * @version    SVN: $Id$
+ */
+abstract class PluginProductCategoryTranslationFormFilter extends BaseProductCategoryTranslationFormFilter
+{
+}
